3e9af65667e910bc205342d31f4195c78f471d9e
Pijush <pijush@example.com>
2026-04-08 20:50:59 +0530

    fix: replace remaining bcrypt with bcryptjs in controller

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { cookieToken } = require('../utils/cookieToken');
 
 exports.register = async (req, res) => {
