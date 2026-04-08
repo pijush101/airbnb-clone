7be139e241bc0359cf4fdb1e65b9737e05fa51e0
Pijush <pijush@example.com>
2026-04-08 13:11:04 +0530

    fix mongoose connection options

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const connectWithDb = () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(process.env.DB_URL)
     .then(console.log("DB connected successfully"))
     .catch((err) => {
         console.log('DB is failed');
